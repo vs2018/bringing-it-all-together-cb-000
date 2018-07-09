4093454fbc7adc4cc3c6faf85a55f68f85968e70
Vishal Sharma <vsh1819@gmail.com>
2018-07-09 19:59:04 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,6 +10,15 @@ class Dog
   end
   
   def self.create_table
+    sql = <<-SQL
+          CREATE TABLE IF NOT EXISTS students(
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          grade TEXT
+          );
+          SQL
     
+    DB[:conn].execute(sql)
+  
   end
 end
