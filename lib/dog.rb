bcc7c368675370ea1bc1ab31d98d35a098d38652
Vishal Sharma <vsh1819@gmail.com>
2018-07-09 20:30:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,7 +45,16 @@ class Dog
     
   end
   
-  def 
+  def self.find_or_create_by(name:, album:)
+    song = DB[:conn].execute("SELECT * FROM songs WHERE name = ? AND album = ?", name, album)
+    if !song.empty?
+      song_data = song[0]
+      song = Song.new(song_data[0], song_data[1], song_data[2])
+    else
+      song = self.create(name: name, album: album)
+    end
+    song
+  end 
   
   def self.new_from_db(row)
   new_dog = self.new(id: row[0], name: row[1], breed: row[2])
