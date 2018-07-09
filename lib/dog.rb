c86742e72c500a45eec34127b6131a62dbbb30b0
Vishal Sharma <vsh1819@gmail.com>
2018-07-09 20:32:04 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -80,6 +80,9 @@ class Dog
   end
   
   def self.find_by_id(id)
+     if self.id
+       self.update
+     else
      sql = <<-SQL
       SELECT *
       FROM dogs
@@ -90,6 +93,7 @@ class Dog
     DB[:conn].execute(sql, id).map do |row|
       self.new_from_db(row)
     end.first
+  end
   end
   
   
